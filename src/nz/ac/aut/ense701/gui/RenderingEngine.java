299a6216490f37efef282f2974de4241152a05a5
donnacello <donnadychinco@gmail.com>
2017-05-03 09:32:58 +1200

    Change some render methods to be readable
    render methods for the player icon, quest, stamina, inventory, and action boxes

--- src/nz/ac/aut/ense701/gui/RenderingEngine.java
+++ src/nz/ac/aut/ense701/gui/RenderingEngine.java
@@ -38,8 +38,31 @@ public class RenderingEngine {
     GameState gamestate;
     Game game;
 
+    ScalingAssistant scaleAssist;
+    
+    //array indexes reference
+    private final int X_OFFSET, Y_OFFSET, IMG_WIDTH, IMG_HEIGHT, BOX_1, BOX_2, BOX_3;
+    
+    //X offset, Y offset, width, and height values of side panel images
+    private final int[] PLAYER_ICON = {12,12,170,155};
+    private final int[] CLIPBOARD_ICON = {182,12,105,155};
+    private final int[] STAMINA_BAR = {45,170,100,20}; //width is game dependent
+    private final int[] INVENTORY_BOXES = {35,225,65,65,35,115,195};
+    private final int[] ACTION_BOXES = {35,315,65,65,35,115,195};
+    private final int[] BOXES_X_OFFSET = {35,115,195};
+    
+    
+
     public RenderingEngine(GameLoop loop) {
+        X_OFFSET = 0;
+        Y_OFFSET = 1;
+        IMG_WIDTH = 2; 
+        IMG_HEIGHT = 3;
+        BOX_1 = 0;
+        BOX_2 = 1;
+        BOX_3 = 2;
         this.loop = loop;
+        this.scaleAssist = ScalingAssistant.getScalingAssistant();
     }
 
     /**
@@ -106,11 +129,11 @@ public class RenderingEngine {
     private void renderSidePanel(Graphics2D g2d, SidePanel sidePanel) {
         ScalingAssistant scaleAssist = ScalingAssistant.getScalingAssistant();
         renderBoards(g2d);
-        renderPlayerIcon(g2d, sidePanel, scaleAssist);
-        renderQuest(g2d, sidePanel, scaleAssist);
-        renderStaminaBar(g2d, sidePanel, scaleAssist);
-        renderInventoryGroup(g2d, sidePanel, scaleAssist);
-        renderActionGroup(g2d, sidePanel, scaleAssist);
+        renderPlayerIcon(g2d, sidePanel);
+        renderQuest(g2d, sidePanel);
+        renderStaminaBar(g2d, sidePanel);
+        renderInventoryGroup(g2d, sidePanel);
+        renderActionGroup(g2d, sidePanel);
 
     }
 
@@ -130,6 +153,12 @@ public class RenderingEngine {
                 GUIConfigs.boardWidth(), GUIConfigs.boardHeight()));
     }
 
+        
+    
+    private int valueOf(int[] array, int index){
+        return scaleAssist.scale(array[index]);
+    }
+
     /**
      * Renders a Player Icon for the Side Panel
      *
@@ -137,11 +166,11 @@ public class RenderingEngine {
      * @param sidePanel side panel object
      * @param scAs scaling assistant
      */
-    private void renderPlayerIcon(Graphics2D g2d, SidePanel sidePanel, ScalingAssistant scAs) {
+    private void renderPlayerIcon(Graphics2D g2d, SidePanel sp) {
         //display images
-        g2d.drawImage(sidePanel.showPlayerIcon(),
-                scAs.scale(12), scAs.scale(12), //X & Y offset
-                scAs.scale(170), scAs.scale(155), //width & height
+        g2d.drawImage(sp.showPlayerIcon(),
+                valueOf(PLAYER_ICON,X_OFFSET), valueOf(PLAYER_ICON,Y_OFFSET),
+                valueOf(PLAYER_ICON,IMG_WIDTH), valueOf(PLAYER_ICON,IMG_HEIGHT),
                 null);
     }
 
@@ -152,20 +181,25 @@ public class RenderingEngine {
      * @param sidePanel side panel object
      * @param scAs scaling assistant
      */
-    private void renderQuest(Graphics2D g2d, SidePanel sidePanel, ScalingAssistant scaleAssist) {
+    private void renderQuest(Graphics2D g2d, SidePanel sidePanel) {
+        //setup quest object values
+        int fontSize = scaleAssist.scale(18);
+        int textLeftMargin = scaleAssist.scale(65 + CLIPBOARD_ICON[X_OFFSET] );
+        int kiwiTextTopMargin = scaleAssist.scale(78 + CLIPBOARD_ICON[Y_OFFSET] );
+        int predatorTextTopMargin = scaleAssist.scale(125 + CLIPBOARD_ICON[Y_OFFSET] );
         //displays the quest clipboard image
         g2d.drawImage(sidePanel.showQuests(),
-                scaleAssist.scale(12 + 170), scaleAssist.scale(12), //X & Y offset 
-                scaleAssist.scale(105), scaleAssist.scale(155), //width & height
+                valueOf(CLIPBOARD_ICON,X_OFFSET), valueOf(CLIPBOARD_ICON,Y_OFFSET),
+                valueOf(CLIPBOARD_ICON,IMG_WIDTH), valueOf(CLIPBOARD_ICON,IMG_HEIGHT),
                 null);
+        //set the text color as gray
         g2d.setColor(Color.gray);
-        g2d.setFont(new Font("Arial", Font.BOLD, scaleAssist.scale(18)));
+        //set font properties
+        g2d.setFont(new Font("Arial", Font.BOLD, fontSize));
         //display number of Kiwis to tag/count
-        g2d.drawString(sidePanel.numOfKiwi(),
-                scaleAssist.scale(12 + 170 + 65), scaleAssist.scale(90));//X & Y offset
+        g2d.drawString(sidePanel.numOfKiwi(), textLeftMargin, kiwiTextTopMargin);
         //display number of Predators left
-        g2d.drawString(sidePanel.numOfPredator(),
-                scaleAssist.scale(12 + 170 + 65), scaleAssist.scale(137));//X & Y offset
+        g2d.drawString(sidePanel.numOfPredator(),textLeftMargin,predatorTextTopMargin);
     }
 
     /**
@@ -175,39 +209,48 @@ public class RenderingEngine {
      * @param sidePanel side panel object
      * @param scAs scaling assistant
      */
-    private void renderStaminaBar(Graphics2D g2d, SidePanel sidePanel, ScalingAssistant scaleAssist) {
+    private void renderStaminaBar(Graphics2D g2d, SidePanel sidePanel) {
+        //setup stamina bar values
+        int maxStaminawidth = scaleAssist.scale(sidePanel.totalStamina() * 2);
+        int currentStaminawidth = scaleAssist.scale(sidePanel.currentStamina() * 2);
+        int textLeftMargin = scaleAssist.scale(48);
+        int textTopMargin = scaleAssist.scale(188);
         //display Max Stamina
         g2d.setColor(Color.lightGray);
-        g2d.fill(new Rectangle2D.Double(scaleAssist.scale(45), scaleAssist.scale(170), //X & Y offset
-                scaleAssist.scale(sidePanel.totalStamina() * 2), scaleAssist.scale(20)));//width & height
+        g2d.fill(new Rectangle2D.Double(
+                valueOf(STAMINA_BAR,X_OFFSET), valueOf(STAMINA_BAR,Y_OFFSET),
+                maxStaminawidth, valueOf(STAMINA_BAR,IMG_HEIGHT)));
         //displays Current Stamina
-        g2d.setColor(new Color(57, 181, 75));
-        g2d.fill(new Rectangle2D.Double(scaleAssist.scale(45), scaleAssist.scale(170), //X & Y offset
-                scaleAssist.scale(sidePanel.currentStamina() * 2), scaleAssist.scale(20)));//width & height
+        g2d.setColor(new Color(57, 181, 75));//rgb color values
+        g2d.fill(new Rectangle2D.Double(
+                valueOf(STAMINA_BAR,X_OFFSET), valueOf(STAMINA_BAR,Y_OFFSET),
+                currentStaminawidth, valueOf(STAMINA_BAR,IMG_HEIGHT)));
         //displays the text STAMINA
         g2d.setColor(Color.darkGray);
-        g2d.drawString("STAMINA", scaleAssist.scale(48), scaleAssist.scale(188));
+        g2d.drawString("STAMINA", textLeftMargin, textTopMargin);
     }
 
-    private void renderInventoryGroup(Graphics2D g2d, SidePanel sidePanel, ScalingAssistant scaleAssist) {
+    
+    private void renderInventoryGroup(Graphics2D g2d, SidePanel sidePanel) {
+        int textLeftMargin = scaleAssist.scale(35);
+        int textTopMargin = scaleAssist.scale(220);
         //display inventory boxes
         g2d.setColor(Color.gray);
-        g2d.drawString("INVENTORY", 
-                        scaleAssist.scale(35), scaleAssist.scale(220));//X & Y offset
+        g2d.drawString("INVENTORY", textLeftMargin, textTopMargin);
             //inventory box 1
-            g2d.drawImage(sidePanel.inventoryImage()[0], 
-                    scaleAssist.scale(35), scaleAssist.scale(225), //X & Y offset 
-                    scaleAssist.scale(65), scaleAssist.scale(65), //width & height
+            g2d.drawImage(sidePanel.inventoryImage()[BOX_1], 
+                    valueOf(BOXES_X_OFFSET,BOX_1), valueOf(INVENTORY_BOXES,Y_OFFSET),
+                    valueOf(INVENTORY_BOXES,IMG_WIDTH), valueOf(INVENTORY_BOXES,IMG_HEIGHT),
                     null);
             //inventory box 2
-            g2d.drawImage(sidePanel.inventoryImage()[1], 
-                    scaleAssist.scale(35+65+15), scaleAssist.scale(225), //X & Y offset 
-                    scaleAssist.scale(65), scaleAssist.scale(65), //width & height
+            g2d.drawImage(sidePanel.inventoryImage()[BOX_2], 
+                    valueOf(BOXES_X_OFFSET,BOX_2), valueOf(INVENTORY_BOXES,Y_OFFSET),
+                    valueOf(INVENTORY_BOXES,IMG_WIDTH), valueOf(INVENTORY_BOXES,IMG_HEIGHT),
                     null);
             //inventory box 3
-            g2d.drawImage(sidePanel.inventoryImage()[2], 
-                    scaleAssist.scale(35+(65*2)+30), scaleAssist.scale(225), //X & Y offset 
-                    scaleAssist.scale(65), scaleAssist.scale(65), //width & height
+            g2d.drawImage(sidePanel.inventoryImage()[BOX_3], 
+                    valueOf(BOXES_X_OFFSET,BOX_3), valueOf(INVENTORY_BOXES,Y_OFFSET),
+                    valueOf(INVENTORY_BOXES,IMG_WIDTH), valueOf(INVENTORY_BOXES,IMG_HEIGHT),
                     null);
     }
     
@@ -218,24 +261,26 @@ public class RenderingEngine {
      * @param sidePanel
      * @param scaleAssist
      */
-    private void renderActionGroup(Graphics2D g2d, SidePanel sidePanel, ScalingAssistant scaleAssist) {
-        //display action boxes    
-        g2d.drawString("ACTION",
-                scaleAssist.scale(35), scaleAssist.scale(310));//X & Y offset
+    private void renderActionGroup(Graphics2D g2d, SidePanel sidePanel) {
+        //setup text margins
+        int textLeftMargin = scaleAssist.scale(35);
+        int textTopMargin = scaleAssist.scale(310);   
+        //show action text
+        g2d.drawString("ACTION", textLeftMargin, textTopMargin);
         //action box tag
-        g2d.drawImage(sidePanel.actionImage()[0],
-                scaleAssist.scale(35), scaleAssist.scale(315), //X & Y offset 
-                scaleAssist.scale(65), scaleAssist.scale(65), //width & height
+        g2d.drawImage(sidePanel.actionImage()[BOX_1],
+                valueOf(BOXES_X_OFFSET,BOX_1), valueOf(ACTION_BOXES,Y_OFFSET),
+                valueOf(ACTION_BOXES,IMG_WIDTH), valueOf(ACTION_BOXES,IMG_HEIGHT),
                 null);
         //action box trap
-        g2d.drawImage(sidePanel.actionImage()[1],
-                scaleAssist.scale(35 + 65 + 15), scaleAssist.scale(315), //X & Y offset 
-                scaleAssist.scale(65), scaleAssist.scale(65), //width & height
+        g2d.drawImage(sidePanel.actionImage()[BOX_2],
+                valueOf(BOXES_X_OFFSET,BOX_2), valueOf(ACTION_BOXES,Y_OFFSET),
+                valueOf(ACTION_BOXES,IMG_WIDTH), valueOf(ACTION_BOXES,IMG_HEIGHT),
                 null);
         //action box collect
-        g2d.drawImage(sidePanel.actionImage()[2],
-                scaleAssist.scale(35 + (65 * 2) + 30), scaleAssist.scale(315), //X & Y offset 
-                scaleAssist.scale(65), scaleAssist.scale(65), //width & height
+        g2d.drawImage(sidePanel.actionImage()[BOX_3],
+                valueOf(BOXES_X_OFFSET,BOX_3), valueOf(ACTION_BOXES,Y_OFFSET),
+                valueOf(ACTION_BOXES,IMG_WIDTH), valueOf(ACTION_BOXES,IMG_HEIGHT),
                 null);
     }
 
