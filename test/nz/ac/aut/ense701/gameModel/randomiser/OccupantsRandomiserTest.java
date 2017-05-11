772eaa4733ec71a6d30e86fae7d807e30935afaf
NightRare <vinceyuan15@live.com>
2017-05-12 03:37:20 +1200

    Import mockito

---
+++ test/nz/ac/aut/ense701/gameModel/randomiser/OccupantsRandomiserTest.java
@@ -0,0 +1,11 @@
+package nz.ac.aut.ense701.gameModel.randomiser;
+
+import nz.ac.aut.ense701.gameModel.IDataManager;
+import org.mockito.Mock;
+
+/**
+ * Created by Yuan on 2017/5/12.
+ */
+public class OccupantsRandomiserTest {
+
+}
